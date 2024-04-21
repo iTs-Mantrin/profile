5bfb674d28887b2f4e35a29c82e45e69e68be4a5
iTs-Mantrin <103366230+iTs-Mantrin@users.noreply.github.com>
2024-04-21 18:52:10 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,10 +32,10 @@ export default function Footer() {
   return (
     <footer className='bg-[#000] text-[#fff]'>
         <div className='max-w-[120rem] w-[92%] m-auto'>
-            <div className='flex py-[4rem]'>
-              <div className='order-2 ml-auto'>
-                <h2 className='text-[18px] uppercase font-bold pb-[1.5rem] tracking-[1px]'>Social</h2>
-                  <div className='flex gap-4 text-[20px]'>
+            <div className='flex py-[4rem] max-sm:flex-col'>
+              <div className='order-2 max-sm:order-1 ml-auto max-sm:m-0'>
+                <h2 className='text-[18px] uppercase font-bold pb-[1.5rem] max-sm:pb-5 tracking-[1px]'>Social</h2>
+                  <div className='flex gap-4 max-sm:gap-9 text-[20px] max-sm:pb-10'>
                     {
                       social.map(
                         (link,index) => {
@@ -45,11 +45,11 @@ export default function Footer() {
                     }
                   </div>
               </div>
-              <div className='order-1 w-[40%]'>
+              <div className='order-1 max-sm:order-2 w-[40%] max-sm:w-full'>
                 <h2 className='text-[18px] uppercase font-bold pb-[1.5rem] tracking-[1px]'>
                   <a target='_blank' rel='noopener noreferrer' href="https://in.linkedin.com/in/itsmantrin?trk=public_profile_browsemap">Manish Kumar</a>
                 </h2>
-                <p className='text-[15px]'>
+                <p className='text-[15px] max-sm:text-[13px]'>
                 A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product
                 </p>
               </div>
