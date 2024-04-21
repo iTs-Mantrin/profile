18e043cd7ea1cf67af09a00b6f66444b077ee7ee
iTs-Mantrin <103366230+iTs-Mantrin@users.noreply.github.com>
2024-04-21 16:31:34 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,27 +28,27 @@ export default function Projects() {
         },
     ]
   return (
-    <section id="projects" className=' py-[6rem]'>
+    <section id="projects" className=' py-[6rem] max-sm:py-5'>
         <div className='max-w-[120rem] w-[92%] m-auto'>
             <div className='text-center mb-[4rem]'>
-                <h2 className='text-[35px] uppercase font-bold tracking-[1px] pb-[4rem]'>Projects</h2>
-                <p className='text-center text-[20px] text-[#555555] max-w-[80rem] w-[80%] m-auto'>Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
+                <h2 className='text-[35px] uppercase font-bold tracking-[1px] pb-[4rem] max-sm:text-[30px] max-sm:pb-3'>Projects</h2>
+                <p className='text-center text-[20px] text-[#555555] max-w-[80rem] w-[80%] m-auto max-sm:w-full m-auto max-sm:text-[16px]'>Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
             </div>
             <div>
                 {
                     projects.map(
                         (project,index) => {
                             return(
-                                <div key={index} className='flex max-w-[100rem] gap-10 py-[4rem]'>
-                                    <div className='w-[50%] px-[10px]'>
+                                <div key={index} className='flex max-w-[100rem] gap-10 py-[4rem] max-sm:flex-col'>
+                                    <div className='w-[50%] px-[10px] max-sm:w-full'>
                                         <img src={project.image} alt={project.name} />
                                     </div>
-                                    <div className='w-[50%] px-[10px]'>
-                                        <h3 className='text-[25px] font-bold tracking-[1px] py-[1rem]'>{project.name}</h3>
-                                        <p className='text-[18px] text-[#555555]'>{project.description}</p>
+                                    <div className='w-[50%] px-[10px] max-sm:w-full'>
+                                        <h3 className='text-[25px] font-bold tracking-[1px] py-[1rem]  max-sm:text-[20px]'>{project.name}</h3>
+                                        <p className='text-[18px] text-[#555555] max-sm:text-[14px]'>{project.description}</p>
                                         <div className='flex py-[40px] space-x-10 items-center'>
-                                            <a href={project.visitNow} target='_blank' rel='noopener noreferrer' className='text-[15px] text-[#fff] bg-[#7843e9] uppercase font-bold py-[15px] px-[60px] rounded tracking-[1px] shadow-[0_5px_15px_0_rgba(0, 0, 0, .15)] hover:translate-y-[-3px] duration-300'>Visit Now</a>
-                                            <a href={project.caseStudty} target='_blank' rel='noopener noreferrer'  className='text-[15px] text-[#fff] bg-[#7843e9] uppercase font-bold py-[15px] px-[60px] rounded tracking-[1px] shadow-[0_5px_15px_0_rgba(0, 0, 0, .15)] hover:translate-y-[-3px] duration-300'>Case Study</a>
+                                            <a href={project.visitNow} target='_blank' rel='noopener noreferrer' className='text-[15px] max-sm:text-[10px] text-[#fff] bg-[#7843e9] uppercase font-bold py-[15px] px-[60px] max-sm:px-8 max-sm:py-3 rounded tracking-[1px] shadow-[0_5px_15px_0_rgba(0, 0, 0, .15)] hover:translate-y-[-3px] duration-300'>Visit Now</a>
+                                            <a href={project.caseStudty} target='_blank' rel='noopener noreferrer'  className='text-[15px] max-sm:text-[10px] text-[#fff] bg-[#7843e9] uppercase font-bold py-[15px] px-[60px] max-sm:px-8 max-sm:py-3 rounded tracking-[1px] shadow-[0_5px_15px_0_rgba(0, 0, 0, .15)] hover:translate-y-[-3px] duration-300'>Case Study</a>
                                         </div>
                                     </div>
                                 </div>
